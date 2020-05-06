c528e2a9e20f5818cb7411ced7e5cde0e7ad7c6b
Salinger MS <minsiua@gmail.com>
2020-05-06 21:16:41 +0900

    ADD follower data saver

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -6,6 +6,8 @@ public class DataManager : MonoBehaviour
 {
     public static DataManager Instance { get; private set; }
 
+    private const string USER_DATA_PATH = "/UserData.json";
+
     public int Followers;
 
     void Awake()
@@ -21,22 +23,47 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        LoadData();
     }
 
     public int AddFollowers(int likes)
     {
-        Debug.Log("Likes: " + likes);
-        Debug.Log("Add Follower: " + likes / 3);
         Followers = Followers + likes / 3;
+        SaveData();
+
         return Followers;
+    }
+    private void LoadData()
+    {
+        if (System.IO.File.Exists(Application.persistentDataPath + USER_DATA_PATH))
+        {
+            string _dataToString = System.IO.File.ReadAllText(Application.persistentDataPath + USER_DATA_PATH);
+            SaveObject loadedObject = JsonUtility.FromJson<SaveObject>(_dataToString);
+            Followers = loadedObject.followers;
+        }
+        else
+        {
+            Debug.Log("no file");
+        }
+        
+    }
+
+    private void SaveData()
+    {
+        SaveObject saveObject = new SaveObject
+        {
+            followers = Followers,
+        };
+        string _jsonData = JsonUtility.ToJson(saveObject);
+        System.IO.File.WriteAllText(Application.persistentDataPath + USER_DATA_PATH, _jsonData);
+    }
+
+    private class SaveObject
+    {
+        public int followers;
     }
 
 }
+
