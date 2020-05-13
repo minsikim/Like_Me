3ec1a9c87603e0d1ef0a86864ac8cf54929efd2b
Salinger MS <minsiua@gmail.com>
2020-05-13 22:13:47 +0900

    ADD imagelist data

---
+++ Assets/Scripts/Data/ImageListData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[CreateAssetMenu(fileName = "ImageListData", menuName = "Project L/ImageListData")]
+public class ImageListData : ScriptableObject
+{
+    public List<Image> Images;
+
+    public Image GetRandom()
+    {
+        return Images[UnityEngine.Random.Range(0, Images.Count)];
+    }
+}
