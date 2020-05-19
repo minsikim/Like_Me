0e8ab41c24826894ff14ff0f8776125fcf77b1e7
Salinger MS <minsiua@gmail.com>
2020-05-19 11:35:43 +0900

    Revert "ADD firebase functionality & DELETE unused packages"
    This reverts commit 150b27de886e2aa2697ed7b8401e8d950b5781ea.

---
+++ Assets/Scripts/Server/RegisterPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterPlayer : MonoBehaviour
+{
+    public InputField displayNameInput, userNameInput, passwordInput;
+    public void onRegisterPlayerButtonClick()
+    {
+        new GameSparks.Api.Requests.RegistrationRequest()
+            .SetDisplayName(displayNameInput.text)
+            .SetPassword(passwordInput.text)
+            .SetUserName(userNameInput.text)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log("Auth Success: "+response.AuthToken);
+                }
+                else
+                {
+                    Debug.Log("Auth Failed: " + response.Errors.JSON.ToString());
+                }
+            }
+            );
+    }
+}
